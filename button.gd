8b5d8029818ca3a64564ef8fd2facdb6862d166f
Alys <a88952598@gmail.com>
2026-05-21 20:38:02 +0200

    Script pour le Pickup fini !

--- button.gd
+++ button.gd
@@ -6,5 +6,15 @@ extends Area3D
 func activationindic(body: Node3D) -> void:
 	indic.visible=true
 	
+	
 func désinctivationindic(body: Node3D) -> void:
 	indic.visible=false
+
+func _process(delta: float) -> void:
+	if indic.visible:
+		if Input.is_action_just_pressed("Pickup"):
+			var node:Node3D=get_parent()
+			node.reparent(inv)
+			node.scale=Vector3(0.2,0.2,0.2)
+			node.position=Vector3(0,inv.get_child_count()*0.3,0)
+			process_mode=Node.PROCESS_MODE_DISABLED
